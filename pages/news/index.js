196870d56b00f6b720ef8cca829fd0ea2250642f
= <=>
2021-08-14 18:10:14 +0530

    Completed

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,16 @@
+import { Fragment } from "react";
+import Link from 'next/link';
+
 const NewsPage = () => {
-    return <h1>The News Page</h1>
+    return <Fragment>
+        <h1>The News Page</h1>
+        <ul>
+            <li><Link href = "/news/nextjs-is-a-great-framework">
+                NextJS is a gret community.
+            </Link></li>
+            <li>Something Else</li>
+        </ul>
+    </Fragment>
 }
 
 export default NewsPage;
